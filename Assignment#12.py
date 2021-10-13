853a48d27ff1046cba26ed15006f94d284a10b1f
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-10-13 22:18:25 +0500

    Add files via upload

---
+++ Assignment#12.py
@@ -0,0 +1,21 @@
+# Problem Statement
+#    1- Create a program to print any user defined multiplication table
+#    2- Print the same above table in reverse order
+
+user_define = int(input("Enter any Number : "))
+for i in range(1,11):
+    print(f"{user_define} * {i} = {user_define * i}")
+
+# <--------------- OUTPUT --------------->  
+
+# Enter a number : 15
+# 15 * 1 = 15
+# 15 * 2 = 30
+# 15 * 3 = 45
+# 15 * 4 = 60
+# 15 * 5 = 75
+# 15 * 6 = 90
+# 15 * 7 = 105
+# 15 * 8 = 120
+# 15 * 9 = 135
+# 15 * 10 = 150
