abcddadf1f37899a8f02c9f4dc91939851646280
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-10-15 22:10:33 +0500

    Update Assignment#14.py

--- Assignment#14.py
+++ Assignment#14.py
@@ -5,3 +5,21 @@
 #     * * * 
 #    * * * * 
 #   * * * * *
+
+
+
+for i in range(5):
+    for j in range(-6, -i):
+        print(" ", end="")
+    for k in range(i+1):
+        print("* ", end="")
+    print()
+
+
+# <--------------- OUTPUT --------------->  
+
+#       * 
+#      * * 
+#     * * *
+#    * * * *
+#   * * * * *
