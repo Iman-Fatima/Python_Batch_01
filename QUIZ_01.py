9dab146b304f9fd5ae7feeeb68c636df3c0069e4
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-10-18 19:06:48 +0500

    Add files via upload

---
+++ QUIZ_01.py
@@ -0,0 +1,65 @@
+# QUIZ_01
+# Problem Statements
+# Q - 01
+# Write a program that will take three integers as user defined inputs and prints the maximum number out of them.
+# Solution:-
+
+
+number_1 = (input("Enter the 1st Number: "))
+number_2 = (input("Enter the 2nd Number: "))
+number_3 = (input("Enter the 3rd Number: "))
+
+if (number_1 >= number_2) and (number_1 >= number_3):
+   maximum = number_1
+elif (number_2 >= number_1) and (number_2 >= number_3):
+   maximum = number_2
+else:
+   maximum = number_3
+
+print("The Maximum Number Is", maximum)
+
+# <--------------- OUTPUT --------------->  
+
+# Enter the 1st Number: 55
+# Enter the 2nd Number: 78
+# Enter the 3rd Number: 95
+# The Maximum Number Is 95
+
+
+
+# Q - 02
+# Write a program to calculate Area of Circle by taking radius as input.
+# Solution:-
+
+
+# Q - 03
+# Write a program to calculate percentage of total marks obtained in final term for a student.
+# Solution:-
+# Q - 04
+# Write a program to print whether the user defined number is a prime number or not with proper message.
+# Solution:-
+# Q - 05
+# Write a Python program to check whether an alphabet is a vowel or consonant.
+# Solution:-
+# Q - 06
+# Write a Python program to sum up two user defined integers. However, if the sum is:
+#     Below Zero - Display "Bigger number was negative"
+#     Equal to ZERO - Display "Both numbers were equally distant on number line on opposite sides"
+#     Greater than ZERO - Display "Bigger number was positive"
+# Solution:-
+# Q - 07
+# Write a Python program to check whether two user defined integers are additive inverse of each other.
+# Q - 08
+# Write a Python program to check whether two user defined integers are multiplicative inverse of each other.
+# Solution:-
+# Q - 09
+# Write a Python program to check a triangle is equilateral, isosceles or scalene.
+# Note :
+#     All triangles have three sides
+#     An equilateral triangle is a triangle in which all three sides are equal
+#     A scalene triangle is a triangle that has three unequal sides
+#     An isosceles triangle is a triangle with (at least) two equal sides
+# Solution:-
+# Q - 10
+# Write a Python program to print user defined multiplication table and also reverse the table.
+# Solution:-
