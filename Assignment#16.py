a1dbda6f4bab8cd4a8c59346ab986728467e15ba
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-10-30 15:01:20 +0500

    Add files via upload

---
+++ Assignment#16.py
@@ -0,0 +1,47 @@
+# Problem Statements
+
+#     Reverse the string to make it readable
+#         ?elbadaer ti ekam uoy naC
+
+# my_string = '?elbadaer ti ekam uoy naC'
+# print(my_string[::-1]) 
+
+# <--------------- OUTPUT --------------->  
+
+# Can you make it readable?
+# .....................................................................................
+#     Assign each word in the given string to a different variable using slicing
+#         Slice me if you can.
+
+# my_string = "Slice me if you can."
+# print(my_string[0:5])
+# print(my_string[6:9])
+# print(my_string[9:12])
+# print(my_string[12:16])
+# print(my_string[16:19])
+
+# <--------------- OUTPUT --------------->  
+
+# Slice
+# me 
+# if
+# you
+# can
+# .....................................................
+
+#     Find number of characters in the given string
+#         Can you tell me how many characters are there using python?
+
+my_string = "Can you tell me how many characters are there in the string by using python?"
+print(f"The given string has {len(my_string)} characters.")
+
+# <--------------- OUTPUT --------------->  
+
+# The given string has 76 characters.
+
+#     Find middle character of the given string
+#         Hey! You can use a built-in python method to find the middle element. 
+#         Test your logics using python. Best of Luck!.
+
+# my_string= "Hey! You can use a built-in python method to find the middle element."
+
